2539ccbc4d26eddc690b0f1392e26946c63b9552
Haxu <nguyenngocminh190901@gmail.com>
2023-04-08 14:01:29 +0700

    return comments without populate owner

--- src/services/CommentService.ts
+++ src/services/CommentService.ts
@@ -24,13 +24,14 @@ class CommentService {
 
     static async getCommentsByPostId(postId: string) {
         const comments = await Comment.find({ post: postId })
-            .select('-__v')
-            .populate([
-                {
-                    path: 'owner',
-                    select: '-password -__v -role -saved_posts -liked_posts',
-                },
-            ]);
+            .sort({ createdAt: 'desc' })
+            .select('-__v');
+        // .populate([
+        //     {
+        //         path: 'owner',
+        //         select: '-password -__v -role -saved_posts -liked_posts',
+        //     },
+        // ]);
 
         return comments;
     }
